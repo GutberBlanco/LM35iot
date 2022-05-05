b49744018d808379d3e3389ba83d58254abcf4b2
GutberBlanco <100219714+GutberBlanco@users.noreply.github.com>
2022-05-05 09:21:15 -0400

    Add files via upload

---
+++ config.h
@@ -0,0 +1,7 @@
+const char* ssid = "Redmi Note 11";        //Indicamos el nombre de la red WiFi (SSID) a la que queremos conectarnos.
+const char* password =  "note12345";    //Indicamos la contraseña de de red WiFi
+
+/*
+const char* ssid = "xxxxxx";        //Indicamos el nombre de la red WiFi (SSID) a la que queremos conectarnos.
+const char* password = "yyyy";  //Indicamos la contraseña de de red WiFi
+*/
