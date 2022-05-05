453c9f04e7503cc53fa53301f327f4f8016c2b51
GutberBlanco <100219714+GutberBlanco@users.noreply.github.com>
2022-05-05 18:38:19 -0400

    Add files via upload

---
+++ PracticaTemperatura/config.h
@@ -0,0 +1,2 @@
+const char* ssid = "DESKTOP-U6KU352 5037";        //Indicamos el nombre de la red WiFi (SSID) a la que queremos conectarnos.
+const char* password =  "gutber1234";    //Indicamos la contraseña de de red WiFi
