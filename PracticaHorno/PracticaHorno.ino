ae15bd57541156c1c6987254a71f07856548699c
JuancaJcA <avilafloresjc@gmail.com>
2022-05-13 07:54:39 -0400

    Horno

---
+++ PracticaHorno/PracticaHorno.ino
@@ -0,0 +1,119 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "FS.h"
+#include <Arduino_JSON.h>
+#include "config.h"
+
+// Creamos el servidor AsyncWebServer en el puerto 80
+AsyncWebServer server(80);
+
+// Create an Event Source on /events
+AsyncEventSource events("/events");
+
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
+
+// Timer variables
+
+String slider;
+unsigned long lastTime = 0;  
+unsigned long timerDelay = 1000; // actualizacion cada segundo//30000;
+
+
+// Get Sensor Readings and return JSON object
+String getSensorReadings(){
+
+  readings["temperature"] = String(analogRead(A0)*0.3225806352);
+  readings["humidity"] =  String(analogRead(A0)*0.3225806352);
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+
+// Initialize LittleFS
+void initFS() {
+ // Iniciamos  SPIFFS
+  if(!SPIFFS.begin())
+     { Serial.println("ha ocurrido un error al montar SPIFFS");
+       return; }
+}
+
+// Inicializando WiFi
+void initWiFi() {
+// conectamos al Wi-Fi
+  WiFi.begin(ssid, password);
+  // Mientras no se conecte, mantenemos un bucle con reintentos sucesivos
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      // Esperamos un segundo
+      Serial.println("Conectando a la red WiFi..");
+    }
+  Serial.println();
+  Serial.println(WiFi.SSID());
+  Serial.print("Direccion IP:\t");
+  // Imprimimos la ip que le ha dado nuestro router
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  initWiFi();
+  initFS();
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  server.serveStatic("/", SPIFFS, "/");
+
+  // Ruta para cargar el archivo index.html
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(SPIFFS, "/index.html", String(), false, processor);
+            });
+            
+  // Ruta para cargar el archivo style.css
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+            request->send(SPIFFS, "/style.css", "text/css");
+            });
+
+  
+  // Request for the latest sensor readings
+  server.on("/readings", HTTP_GET, [](AsyncWebServerRequest *request){
+    String json = getSensorReadings();
+    request->send(200, "application/json", json);
+    json = String();
+  });
+
+
+  server.on("/SLIDER", HTTP_POST, [](AsyncWebServerRequest *request){
+            slider = request->arg("temperatura");
+            Serial.print("Temperatura: \t");
+            Serial.println(slider);
+            request->redirect("/");
+            });
+
+  events.onConnect([](AsyncEventSourceClient *client){
+    if(client->lastId()){
+      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+    }
+    // send event with message "hello!", id current millis
+    // and set reconnect delay to 1 second
+    client->send("hello!", NULL, millis(), 10000);
+  });
+  server.addHandler(&events);
+
+  // Start server
+  server.begin();
+}
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    // Send Events to the client with the Sensor Readings Every 30 seconds
+    events.send("ping",NULL,millis());
+    events.send(getSensorReadings().c_str(),"new_readings" ,millis());
+    lastTime = millis();
+  }
+}
---
+++ PracticaHorno/config.h
@@ -0,0 +1,2 @@
+const char* ssid = "Juanca";        //Indicamos el nombre de la red WiFi (SSID) a la que queremos conectarnos.
+const char* password =  "pejelagarto";    //Indicamos la contraseña de de red WiFi
